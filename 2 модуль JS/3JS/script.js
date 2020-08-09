cccc8ae668fd001311c321b7c2172cde9ed563fb
HHamster39 <a.shuvarikova@yandex.ru>
2020-08-09 18:56:52 +0700

    3 Занятие,9 августа

---
+++ 2 модуль JS/3JS/script.js
@@ -0,0 +1,10 @@
+let step ="10"
+function game(event){
+
+
+console.log(event.keyCode);
+}
+p1.addEventListener("click",game);
+p2.addEventListener("click",game);
+document.addEventListener("keyup",game);
+
